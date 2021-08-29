ae1ae30bd367b3ffad85688bd54b36dc4e4d64e1
roni116 <75166481+roni116@users.noreply.github.com>
2021-08-29 08:18:11 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,91 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "The SI unit for measuring work and energy is?",
+    answer: "Joul",
+    options: [
+      "Joul",
+      "Watt",
+      "Farad",
+      "Ohm"
+    ]
+  },
+    {
+    numb: 2,
+    question: "An atom with more neutrons than protons is called?",
+    answer: "Isotope",
+    options: [
+      "Compound",
+      "Element",
+      "Isotope",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "If the distance between Sun and Earth becomes double, then the gravitational force on Earth will be?",
+    answer: "One Fourth",
+    options: [
+      "Double",
+      "Same",
+      "Half",
+      "One Fourth"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The unit of force is a?",
+    answer: "Derived Unit",
+    options: [
+      "Basic Unit",
+      "Fundamental Unit",
+      "Derived Unit",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following is not a vector quantity?",
+    answer: "Density",
+    options: [
+      "Acceleration",
+      "Density",
+      "Displacement",
+      "Impulse"
+    ]
+  },
+    {
+      numb: 6,
+      question: "When a body is accelerated, the the velocity will also?",
+      answer: "Increase",
+      options: [
+      "Decrease",
+      "Constant",
+      "Increase",
+      "None of the above"
+      ]
+    },
+    {
+      numb: 7,
+      question: "Which of the following is a vector?",
+      answer: "Area",
+      options: [
+      "Energy",
+      "Electric Current",
+      "Area",
+      "Power"
+      ]
+    },
+    {
+      numb: 8,
+      question: "Charles's law is based on?",
+      answer: "Gas",
+      options: [
+      "Solid",
+      "Liquid",
+      "Gas",
+      "None of the above"
+      ]
+    },
+];
