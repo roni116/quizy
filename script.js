2dbb7b7a9f116ffa897f23155f1e7792e85ca1b6
ry <kalyanmitra166@gmail.com>
2021-08-30 12:29:28 +0530

    15

--- script.js
+++ script.js
@@ -152,15 +152,15 @@ function showResult(){
     const scoreText = result_box.querySelector(".score_text");
     if (userScore > 3){ // if user scored more than 3
         //creating a new span tag and passing the user score number and total question number
-        let scoreTag = '<span>You got <p>'+ userScore +'</p>correct out of<p>'+ questions.length +'</p></span>';
+        let scoreTag = '<span>You got <p>'+ userScore/20*100 +'%</p>correct in this module!</p></span>';
         scoreText.innerHTML = scoreTag;  //adding new span tag inside score_Text
     }
     else if(userScore > 1){ // if user scored more than 1
-        let scoreTag = '<span>You got <p>'+ userScore +'</p>correct out of<p>'+ questions.length +'</p></span>';
+        let scoreTag = '<span>You got <p>'+ userScore/20*100 +'%</p>correct in this module!</p></span>';
         scoreText.innerHTML = scoreTag;
     }
     else{ // if user scored less than 1
-        let scoreTag = '<span>and sorry, You got only <p>'+ userScore +'</p>correct out of <p>'+ questions.length +'</p></span>';
+        let scoreTag = '<span>You got <p>'+ userScore/20*100 +'%</p>correct in this module!</span>';
         scoreText.innerHTML = scoreTag;
     }
 }
